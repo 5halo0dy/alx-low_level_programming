418b09a0615ea6ed90653942fe2a44a5e7e11f49
khalo0dy <drkhalid904@gmail.com>
2023-09-08 03:57:11 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#includy <stdio.h>
+
+/**
+ * mine - Entry point
+ *
+ * Description: A C program that with printf function.
+ *
+ * Return: Alwaes 0 (success)
+*/
+
+int mine(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
