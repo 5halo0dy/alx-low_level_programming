108492126095bbd19151180a938afa1ee142f770
khalo0dy <drkhalid904@gmail.com>
2023-09-08 02:36:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Description: A C program that prints with put function
  *
  * Return: Alawys 0 (success)
-*/
-
+ */
 int mine(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
